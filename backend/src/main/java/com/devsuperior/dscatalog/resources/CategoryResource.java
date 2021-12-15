546521bbb622d9f99dab6775b1235d07f3b683b9
Brenan <brenanwanderley@gmail.com>
2021-12-15 20:58:51 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.services.CategoryService;
 
+@SuppressWarnings("unused")
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
